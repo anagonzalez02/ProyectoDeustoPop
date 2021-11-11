c9b1ac6bb99b9f29e7ca35945eabb82c3cda70a7
AitorArtero <93314392+AitorArtero@users.noreply.github.com>
2021-11-11 13:16:48 +0100

    ventanas

---
+++ DeustoPop/src/DeustoPopVentanas/DeustoPopVentanaBuscador.java
@@ -0,0 +1,67 @@
+package DeustoPopVentanas;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+
+public class DeustoPopVentanaBuscador extends JFrame implements ActionListener {
+
+    private JLabel textoBuscador;           // etiqueta o textoBuscador no editable
+    private JTextField cajaBuscador;        // caja de texto, para insertar datos
+    private JButton botonVolver;          // botonVolver con una determinada accion
+    private JButton botonBuscar;          // botonBuscar con una determinada accion
+
+    
+    public DeustoPopVentanaBuscador() {
+        super();                    // usamos el contructor de la clase padre JFrame
+        configurarVentana();        // configuramos la ventana
+        inicializarComponentes();   // inicializamos los atributos o componentes
+    }
+
+    private void configurarVentana() {
+        this.setTitle("DeustoPop");                   			// colocamos titulo a la ventana
+        this.setSize(500, 200);                                	// colocamos tamanio a la ventana (ancho, alto)
+        this.setLocationRelativeTo(null);                       // centramos la ventana en la pantalla
+        this.setLayout(null);                                   // no usamos ningun layout, solo asi podremos dar posiciones a los componentes
+        this.setResizable(false);                               // hacemos que la ventana no sea redimiensionable
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    // hacemos que cuando se cierre la ventana termine todo proceso
+    }
+
+    private void inicializarComponentes() {
+        // creamos los componentes
+        textoBuscador = new JLabel();
+        cajaBuscador = new JTextField();
+        botonVolver = new JButton();
+        botonBuscar = new JButton();
+        
+        // configuramos los componentes
+        textoBuscador.setText("¿Que producto desea buscar?");   // colocamos un texto a la etiqueta
+        textoBuscador.setBounds(50, 50, 200, 30);				// colocamos posicion y tamanio al texto (x, y, ancho, alto)
+        cajaBuscador.setBounds(50, 80, 300, 35);				// colocamos posicion y tamanio a la caja (x, y, ancho, alto)
+        botonVolver.setText("Volver");					// colocamos un texto al botonVolver
+        botonVolver.setBounds(375, 30, 75, 35);			// colocamos posicion y tamanio al botonVolver (x, y, ancho, alto)
+//        botonVolver.addActionListener(this);			// hacemos que el botonVolver tenga una accion y esa accion estara en esta clase
+        botonBuscar.setText("Buscar");					// colocamos un texto al botonBuscar
+        botonBuscar.setBounds(375, 80, 75, 35);			// colocamos posicion y tamanio al botonBuscar (x, y, ancho, alto)
+//        botonBuscar.addActionListener(this);			// hacemos que el botonBuscar tenga una accion y esa accion estara en esta clase
+        // anyadimos los componentes a la ventana
+        this.add(textoBuscador);
+        this.add(cajaBuscador);
+        this.add(botonVolver);
+        this.add(botonBuscar);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //String busacado = cajaBuscador.getText();                                 // obtenemos el contenido de la caja de texto
+    }
+
+    public static void main(String[] args) {
+    	DeustoPopVentanaBuscador B = new DeustoPopVentanaBuscador();      // creamos una ventana
+        B.setVisible(true);             // hacemos visible la ventana creada
+    }
+}
---
+++ DeustoPop/src/DeustoPopVentanas/DeustoPopVentanaPrincipal.java
@@ -0,0 +1,132 @@
+//no esta acabada
+package DeustoPopVentanas;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
+import java.awt.*;
+import javax.swing.*;
+
+public class DeustoPopVentanaPrincipal extends JFrame implements ActionListener {
+
+	private JScrollPane sPanelPrincipal;
+	private JPanel panelMain;
+	private JPanel panelTop;
+	private JPanel panelMid;
+	private JButton botonVender;
+	private JButton botonFiltrar;
+	private JComboBox comboFiltrar;
+	private JLabel textoSaldo;
+	//No se como hacer lo de las imagenes y que se vayan generando/apareciendo nuevos porductos
+	
+	
+    public DeustoPopVentanaPrincipal() {
+        super();                    // usamos el contructor de la clase padre JFrame
+        configurarVentana();        // configuramos la ventana
+        inicializarComponentes();   // inicializamos los atributos o componentes
+    }
+    
+    private void configurarVentana() {
+        this.setTitle("DeustoPop");                   			// colocamos titulo a la ventana
+        this.setSize(500, 700);                                	// colocamos tamanio a la ventana (ancho, alto)
+        this.setLocationRelativeTo(null);                       // centramos la ventana en la pantalla
+        this.setLayout(new BorderLayout());                                   // no usamos ningun layout, solo asi podremos dar posiciones a los componentes
+        this.setResizable(false);                               // hacemos que la ventana no sea redimiensionable
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    // hacemos que cuando se cierre la ventana termine todo proceso
+        
+    }
+    
+    private void inicializarComponentes() {
+    	panelMain = new JPanel(new BorderLayout());
+    	panelMain.setPreferredSize(new Dimension(466, 650));	//pongo el x en 466 y no en 500 pq si no se me activa el scrollPane y no me ape tenerlo ahí activao
+    	panelMain.setVisible(true);
+    	
+    	panelTop = new JPanel(new BorderLayout());
+    	panelTop.setPreferredSize(new Dimension(500, 50));
+    	panelTop.setVisible(true);
+    	
+    	//crear nuevo panel con gridLayout y meterlo en el CENTER para postear ahí los productos
+    	panelMid = new JPanel(new GridLayout(0,2));
+    	panelMid.setPreferredSize(new Dimension(500, 650));
+    	panelMid.setVisible(true);
+    	
+    	this.add(panelMain);
+    	panelMain.add(panelTop, BorderLayout.NORTH);
+    	panelMain.add(panelMid, BorderLayout.CENTER);
+    	
+    	//el orden impporta pq sta todo dentro del Jpanel
+    	
+    	botonVender = new JButton();
+    	botonVender.setText("Vender");
+    	botonVender.setPreferredSize(new Dimension(100, 40));
+    	//sPanelPrincipal.add(botonVender);
+    	panelTop.add(botonVender, BorderLayout.WEST);
+    	
+    	
+    	botonFiltrar = new JButton();
+    	botonFiltrar.setText("Filtrar");
+    	botonFiltrar.setPreferredSize(new Dimension(100, 40));
+    	//sPanelPrincipal.add(botonFiltrar);
+    	panelTop.add(botonFiltrar, BorderLayout.CENTER);
+    	
+    	/*
+    	//si descomentas esta parte del código tiene q comentar "botonFiltrar" pq si no se solapan
+    	comboFiltrar = new JComboBox();
+    	comboFiltrar.addItem("Color");
+    	comboFiltrar.addItem("Ropa");
+    	comboFiltrar.addItem("Libros");
+    	panelTop.add(comboFiltrar, BorderLayout.CENTER);
+    	*/
+    	
+    	textoSaldo = new JLabel();
+    	textoSaldo.setText("*getSaldo* €");
+    	//textoSaldo.setSize(100, 100);
+    	textoSaldo.setPreferredSize(new Dimension(100, 40));
+    	//sPanelPrincipal.add(textoSaldo);
+    	panelTop.add(textoSaldo, BorderLayout.EAST);
+    	
+    	/*
+    	botonPatata = new JButton();
+    	botonPatata.setText("Patata");
+    	botonPatata.setPreferredSize(new Dimension(100, 40));
+    	//sPanelPrincipal.add(botonFiltrar);
+    	panelMain.add(botonPatata, BorderLayout.CENTER);
+    	*/
+    	
+        panelMid.add(new JButton("Button 1"));
+        panelMid.add(new JButton("Button 2"));
+        panelMid.add(new JButton("Button 3"));
+        panelMid.add(new JButton("Button 4"));
+        //panelMid.add(new JButton("Button 5"));
+
+
+    	
+    	
+    	sPanelPrincipal = new JScrollPane(panelMain);
+    	sPanelPrincipal.setPreferredSize(new Dimension(500, 700));
+    	this.add(sPanelPrincipal, BorderLayout.CENTER);
+    	sPanelPrincipal.setVisible(true);
+    	
+    	
+    }
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+    public static void main(String[] args) {
+    	DeustoPopVentanaPrincipal P = new DeustoPopVentanaPrincipal();      // creamos una ventana
+        P.setVisible(true);             // hacemos visible la ventana creada
+    }
+}
