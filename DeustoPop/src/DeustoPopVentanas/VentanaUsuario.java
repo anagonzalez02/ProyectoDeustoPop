a9b1d4c797544936252552e6246c09da6cc59dda
saaraahdz <sara.hernandez@opendeusto.es>
2021-11-22 23:33:48 +0100

    Mejora clase Usuario

--- DeustoPop/src/DeustoPopVentanas/VentanaUsuario.java
+++ DeustoPop/src/DeustoPopVentanas/VentanaUsuario.java
@@ -1,5 +1,14 @@
 package DeustoPopVentanas;
 
-public class VentanaUsuario {
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class VentanaUsuario extends JFrame {
+	
+	private JLabel nombreUsuario;
+	private JLabel telefonoUsuario;
+	private JLabel emailUsuario;
+	private JLabel saldoUsuario;
+	private JLabel tartjetaEncUsuario;
 
 }
