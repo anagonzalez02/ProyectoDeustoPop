1ea1d3e9bb99793fade358316aba1e23e9eaebb3
VICTOR-HERRERO <victorherrero@opendeusto.es>
2022-01-20 12:12:37 +0100

    eliminado error ventanas

--- DeustoPop/src/ventanas/VentanaLogIn.java
+++ DeustoPop/src/ventanas/VentanaLogIn.java
@@ -128,7 +128,7 @@ public class VentanaLogIn extends JFrame implements ActionListener{
 				} else if (ventanaVolver == "VentanaUsuario") {
 					VentanaPrincipal.main(null);
 	    			dispose();
-					//JOptionPane.showMessageDialog(null, "Para poder entrar al perfil deberás iniciar sesión en DeustoPop.");
+
 				} else if (ventanaVolver == null) {
 					VentanaPrincipal.main(null);
 	    			dispose();
